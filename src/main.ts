871bfe6775109a903ab93f9cfe6556a4a463b5eb
sktguha2 <sguha@confluent.io>
2025-03-08 00:58:44 +0530

    right on edge, release 2.2

--- src/main.ts
+++ src/main.ts
@@ -310,7 +310,7 @@ function initOceanScene(){
     window.aWorldX = aWorldX;
     window.aWorldY = aWorldY;
     const resetF = 7;
-    const boundMult = 1.26;
+    const boundMult = 1.1;
     console.log(camera.rotation, camera.position);
     // New: Wrap camera if it goes beyond world boundaries using full worldX and worldY
     // rem minus
