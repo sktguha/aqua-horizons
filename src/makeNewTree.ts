bd3a24ffc5cfcfb582dad7c342a4181937a70331
sktguha2 <sguha@confluent.io>
2025-03-08 23:13:05 +0530

    ck release 2.2 tree nicely different types and tons of customisations added

--- src/makeNewTree.ts
+++ src/makeNewTree.ts
@@ -28,16 +28,27 @@ import { Tree } from '@dgreenheck/ez-tree';
     return tree;
   }
   const tree = new Tree();
-
   // Set parameters
   tree.options.seed = 12345;
 //   tree.options.trunk.length = 20;
   tree.options.branch.levels = 3;
   function makeNewTree(){
-    
+    const tree = new Tree();
+
+// Set parameters
+tree.options.seed = 12345;
+// tree.options.trunk.length = 20;
+tree.options.branch.levels = 3;
 
 // Generate tree and add to your Three.js scene
-    return tree.generate();
+tree.generate();
+const treeContainer = new THREE.Group();
+    treeContainer.add(tree);
+
+    // Set the group's position
+    treeContainer.position.set( Math.random() * 2000, 0, Math.random() * 2000);
+
+    return treeContainer;
   }
 
   export default makeNewTree;
